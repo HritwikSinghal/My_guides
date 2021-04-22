6b5a92cf3cfe7a021102867743e27196d5e7cfc6
Hritwik <Hritwiksinghal@outlook.in>
2021-04-23 04:23:39 +0530

    add zsh config file

---
+++ src/scripts/ZSH.sh
@@ -0,0 +1,25 @@
+#!/bin/env sh
+
+sudo apt install zsh git -y
+sudo pacman -S zsh zsh-completions micro git --noconfirm --needed
+
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+echo "autoload -U compinit && compinit" >> ~/.zshrc
+
+
+# Does not work
+sudo sed "s/plugins=(/plugins=(zsh-syntax-highlighting zsh-autosuggestions zsh-completions/g" ~/.zshrc
+
+# Powerlevel10K
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+
+# yay -S --noconfirm zsh-theme-powerlevel10k-git
+# echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
+
+

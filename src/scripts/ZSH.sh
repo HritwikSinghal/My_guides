2d9fef56c7a6522000f8529c57e1fce7a81b7b41
Hritwik <Hritwiksinghal@outlook.in>
2021-09-23 06:08:03 +0530

    zsh: fix zsh script

--- src/scripts/ZSH.sh
+++ src/scripts/ZSH.sh
@@ -8,11 +8,11 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
-echo "autoload -U compinit" >> ~/.zshrc
+echo "autoload -U compinit && compinit" >> ~/.zshrc
 
+sudo sed -i "s/# ENABLE_CORRECTION=\"true\"/ENABLE_CORRECTION=\"true\"/g" ~/.zshrc
+sudo sed -i "s/plugins=(git)/plugins=(git zsh-syntax-highlighting zsh-autosuggestions zsh-completions)/g" ~/.zshrc
 
-# Does not work, fix this
-sudo sed "s/plugins=(/plugins=(zsh-syntax-highlighting zsh-autosuggestions zsh-completions/g" ~/.zshrc
 
 # Powerlevel10K
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
@@ -20,6 +20,3 @@ echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
 
 # yay -S --noconfirm zsh-theme-powerlevel10k-git
 # echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
-
-
-

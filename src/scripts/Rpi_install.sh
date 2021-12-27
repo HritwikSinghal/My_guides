870acc3cb5af39d3d5defbadc126fdc0b4ad93d3
Hritwik <Hritwiksinghal@outlook.in>
2021-12-27 16:32:06 +0530

    rpi: update manjaro headless guide on rpi

--- src/scripts/Rpi_install.sh
+++ src/scripts/Rpi_install.sh
@@ -53,13 +53,11 @@ echo "hdmi_mode=82" | sudo tee -a /boot/config.txt
 # --------------------------------------------------------------------------------------------- #
 # --------------------------------------------------------------------------------------------- #
 # --------------------------------------------------------------------------------------------- #
+
 # ------------------------------------ MANJARO Install ---------------------------------------- #
 # --------------------------------------------------------------------------------------------- #
 # --------------------------------------------------------------------------------------------- #
 
-
-
-#!/bin/bash
 
 # GUIDE FOR CHROOTING IN MANJARO RPI INSTALL FROM PC
 
@@ -69,75 +67,82 @@ echo "hdmi_mode=82" | sudo tee -a /boot/config.txt
     # https://unix.stackexchange.com/a/222981
     # https://askubuntu.com/a/855307
     # https://unix.stackexchange.com/a/280534
+    # https://wiki.archlinux.org/title/Iwd
 
 # Extra resources
     # https://www.raspberrypi.org/forums/viewtopic.php?t=233691
     # https://forum.manjaro.org/t/howto-chroot-from-or-into-any-linux-distribution/34071
 
-# yay -S --noconfirm --needed qemu-user-static
-# sudo mount /dev/sdc2 -o rw /mnt                   # mount root partiton
-# cd /mnt
-# sudo mount --bind /dev dev/
-# sudo mount --bind /sys sys/
-# sudo mount --bind /proc proc/
-# sudo mount --bind /dev/pts dev/pts
+# maybe try arch-chroot for easy-chrooting (last tym it didn't work tho)
 
-# sudo cp $(which qemu-arm-static) /mnt/usr/bin
-# sudo systemctl restart systemd-binfmt.service
-# sudo cp /etc/resolv.conf /mnt/etc/resolv.conf     # for enabling networking
-# sudo chroot /mnt qemu-arm-static /bin/bash
+yay -S --noconfirm --needed qemu-user-static-bin
+sudo mount /dev/sdc2 -o rw /mnt
+cd /mnt
+sudo mount --bind /dev /mnt/dev/
+sudo mount --bind /sys /mnt/sys/
+sudo mount --bind /proc /mnt/proc/
+sudo mount --bind /dev/pts /mnt/dev/pts
 
+sudo cp $(which qemu-arm-static) /mnt/usr/bin
+sudo systemctl restart systemd-binfmt.service
 
+# for enabling networking
+sudo cp /etc/resolv.conf /mnt/etc/resolv.conf
+sudo chroot /mnt qemu-arm-static /bin/bash
+# or try - sudo chroot /mnt
+
+# Use iwd for networking
 # Network
-sudo pacman -Syyu --noconfirm --needed networkmanager
-sudo systemctl enable rc-local
-sudo systemctl enable NetworkManager
-sudo systemctl start NetworkManager
-sudo nmcli dev wifi connect Singhals password "123123123"
+# sudo pacman -Syyu --noconfirm --needed networkmanager
+# sudo systemctl enable rc-local
+# sudo systemctl enable NetworkManager
+# sudo systemctl start NetworkManager
+# sudo nmcli dev wifi connect Singhals password "123123123"
+# sudo cp wpa_supplicant-wlan0.conf /mnt/etc/wpa_supplicant/wpa_supplicant-wlan0.conf
+# sudo ln -s /usr/lib/systemd/system/wpa_supplicant\@.service /mnt/etc/systemd/system/multi-user.target.wants/wpa_supplicant
 
-sudo pacman -Syu --noconfirm --needed yay htop neofetch fish python3 cronie
-yay -S --noconfirm --needed micro wl-clipboard git rar unrar p7zip
-yay -S --noconfirm --needed net-tools curl neovim spacevim ranger exa bat ripgrep fd procs
-
+chsh -s /usr/bin/fish
+pacman -Syu --noconfirm --needed yay
+yay -S --noconfirm --needed git htop neofetch fish python3 cronie curl neovim
+yay -S --noconfirm --needed micro wl-clipboard net-tools spacevim ranger exa bat ripgrep fd procs
 pip3 install pipenv
-sudo systemctl enable cronie
-sudo systemctl start cronie
-sudo systemctl enable fstrim.timer
-sudo systemctl start fstrim.timer
+
+systemctl enable cronie
+systemctl enable fstrim.timer
+systemctl enable sshd
+pacman-mirrors -c Global
+
+# rpi uses iwd and NOT wpa_supplicant
+mkdir -p /var/lib/iwd
+touch /var/lib/iwd/Singhals.psk
+# add below to above file (remove # from start)
+#[Security]
+#Passphrase=123123123
 
 
-# pacman, color - Dont append at end of file
-# echo 'ILoveCandy' | sudo tee -a /etc/pacman.conf
-# echo 'Color' | sudo tee -a /etc/pacman.conf
+# finally
+sudo umount /mnt/dev/ -l
+sudo umount /mnt/sys/ -l
+sudo umount /mnt/proc/ -l
+sudo umount /mnt/dev/pts -l
+sudo umount /mnt/ -l
 
 
 
-
-
-
-
-
-
-sudo cp wpa_supplicant-wlan0.conf /run/media/hritwik/ROOT_MNJRO1/etc/wpa_supplicant/wpa_supplicant-wlan0.conf
-
-sudo ln -s /usr/lib/systemd/system/wpa_supplicant\@.service /run/media/hritwik/ROOT_MNJRO1/etc/systemd/system/multi-user.target.wants/wpa_supplicant
-
-
-
-- Wifi
-    - https://unix.stackexchange.com/questions/483678/debian-connect-to-wifi-automatically-when-in-range
-
-    ```
-    /bin/ping -q -c3 1.1.1.1  > /dev/null
-    if [ $? -ne 0 ]
-    then
-    killall wpa_supplicant
-    /bin/ip link set wlan0 up
-    /sbin/wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant.conf  > /dev/null 2>&1
-    /sbin/dhclient -r
-    /sbin/dhclient  > /dev/null 2>&1
-    fi
-
-    ```
-
-    sudo wpa_supplicant -c /etc/wpa_supplicant/wpa_supplicant.conf -i wlan0&
+# - Wifi (for debian based systems that use wpa_supplicant)
+#     - https://unix.stackexchange.com/questions/483678/debian-connect-to-wifi-automatically-when-in-range
+#
+#     ```
+#     /bin/ping -q -c3 1.1.1.1  > /dev/null
+#     if [ $? -ne 0 ]
+#     then
+#     killall wpa_supplicant
+#     /bin/ip link set wlan0 up
+#     /sbin/wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant.conf  > /dev/null 2>&1
+#     /sbin/dhclient -r
+#     /sbin/dhclient  > /dev/null 2>&1
+#     fi
+#
+#     ```
+#
+#     sudo wpa_supplicant -c /etc/wpa_supplicant/wpa_supplicant.conf -i wlan0&
